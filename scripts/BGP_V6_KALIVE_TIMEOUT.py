fa4cb92d5e50f57f3276490903aa0372b31d6b3e
Kikito <nikita.tyunyayev@gmail.com>
2020-11-18 13:38:08 +0100

    updated timeout script

--- scripts/BGP_V6_KALIVE_TIMEOUT.py
+++ scripts/BGP_V6_KALIVE_TIMEOUT.py
@@ -12,12 +12,9 @@ BGP_FRROUTING.sendline('zebra')
 BGP_FRROUTING.sendline('enable')
 BGP_FRROUTING.sendline('configure terminal')
 BGP_FRROUTING.sendline('router bgp')
+BGP_FRROUTING.sendline('no neighbor ' + NEIGHBORSV6 + ' ebgp-multihop')
 BGP_FRROUTING.sendline('neighbor ' + NEIGHBORSV6 + ' timers ' + KALIVE + ' ' + TIMEOUT)
 BGP_FRROUTING.sendline('exit')
-BGP_FRROUTING.sendline('write memory')
-BGP_FRROUTING.sendline('neighbor {} shutdown'.format(NEIGHBORSV6))
-time.sleep(3)
-BGP_FRROUTING.sendline('no neighbor {} shutdown'.format(NEIGHBORSV6))
 BGP_FRROUTING.kill(0)
 
 
